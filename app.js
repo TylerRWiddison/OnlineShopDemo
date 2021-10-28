8d180788c2e82ba1b82d921ce1b1276dd715c68a
TylerRWiddison <tyler.rwiddison@gmail.com>
2021-10-28 14:08:54 -0600

    make it better

---
+++ app.js
@@ -0,0 +1,168 @@
+const path = require("path");
+const fs = require("fs");
+const https = require('https');
+
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const multer = require("multer");
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.47ujr.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
+const app = express();
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync('server.key')
+const certificate = fs.readFileSync('server.cert')
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const authRoutes = require("./routes/auth");
+const shopRoutes = require("./routes/shop");
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "default-src": ["'self'"],
+      "script-src": ["'self'", "'unsafe-inline'", "js.stripe.com"],
+      "style-src": ["'self'", "'unsafe-inline'", "fonts.googleapis.com"],
+      "frame-src": ["'self'", "js.stripe.com"],
+      "font-src": ["'self'", "fonts.googleapis.com", "fonts.gstatic.com"],
+    },
+  })
+);
+// Compression is good for when your hosting provider does not offer compression or when it is your own server you are using
+app.use(compression());
+// Morgan is good for when your hosting provider does not log requests
+app.use(morgan("combined", { stream: accessLogStream }));
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(
+  // secret should be set to a long string in production
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+
+app.use(csrfProtection);
+app.use(flash());
+
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (req.session.isLoggedIn) {
+    req.session.user = new User().init(req.session.user);
+  }
+  console.log("app.js running with request");
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get("/500", errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    // User.findOne().then((user) => {
+    // if (!user) {
+    //   const user = new User({
+    //     name: "Ty",
+    //     email: "ty@test.com",
+    //     cart: {
+    //       items: [],
+    //     },
+    //   });
+    //   user.save();
+    // }
+    // });
+    // https.createServer({key: privateKey, cert: certificate}, app).listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => console.log(err));
